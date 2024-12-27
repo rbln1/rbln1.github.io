18b93776ef6cd139c06381fb0660f7c8b7703e3a
Andrey Dolgushin <rubl.and.dev@gmail.com>
2024-12-27 23:43:03 +0300

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "366ba3ca0a17884cc9581807f4ac8978",
+const RESOURCES = {"flutter_bootstrap.js": "1a663ae7e3109692d53d7e154dc6c950",
 "version.json": "70875cf26614a1aad5489a8873fd2698",
 "index.html": "e3504b933717c8f4ff6a70a56bc31a51",
 "/": "e3504b933717c8f4ff6a70a56bc31a51",
-"main.dart.js": "daf59924d461429f0245c577d674f8bb",
+"main.dart.js": "2ec330bf24b74bc1e56d7287156a4f1b",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
